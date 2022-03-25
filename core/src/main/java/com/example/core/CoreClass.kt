063c980557cc35dec36e01c040b5ae0622189c0c
roman_aimaletdinov <r.aimaletdinov@indriver.com>
2022-03-25 16:37:15 +0300

    init

---
+++ core/src/main/java/com/example/core/CoreClass.kt
@@ -0,0 +1,8 @@
+package com.example.core
+
+class CoreClass {
+
+    fun getFeature(): String {
+        return "CoreClass"
+    }
+}
---
+++ first-feature/src/main/java/com/example/first_feature/FirstFeature.kt
@@ -0,0 +1,8 @@
+package com.example.first_feature
+
+class FirstFeature {
+
+    fun getFeature(): String {
+        return "getFeature"
+    }
+}
