10eb54407e7fafdbb8481893bf633a3b594865e3
roman_aimaletdinov <r.aimaletdinov@indriver.com>
2022-03-25 17:16:44 +0300

    added tests and implementation

--- core/src/main/java/com/example/core/CoreClass.kt
+++ core/src/main/java/com/example/core/CoreClass.kt
@@ -3,10 +3,12 @@ package com.example.core
 class CoreClass {
 
     fun getFeature(): String {
+        Thread.sleep(5000)
         return "CoreClass"
     }
 
     fun getFeature2(): String {
+        Thread.sleep(5000)
         return "CoreClass2"
     }
 }
--- first-feature/src/main/java/com/example/first_feature/FirstFeature.kt
+++ first-feature/src/main/java/com/example/first_feature/FirstFeature.kt
@@ -3,10 +3,12 @@ package com.example.first_feature
 class FirstFeature {
 
     fun getFeature(): String {
+        Thread.sleep(5000)
         return "getFeature"
     }
 
     fun getFeature2(): String {
+        Thread.sleep(5000)
         return "getFeature2"
     }
 }
