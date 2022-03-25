9a2bf428742111b71f64ac5f03df4b146a5e3cec
roman_aimaletdinov <r.aimaletdinov@indriver.com>
2022-03-25 16:38:46 +0300

    init

--- app/src/main/java/com/example/testimpactanalysislib/MainActivity.kt
+++ app/src/main/java/com/example/testimpactanalysislib/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.testimpactanalysislib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.core.CoreClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = CoreClass().getFeature()
+        Log.d("ROMAN", "core: $text")
     }
 }
