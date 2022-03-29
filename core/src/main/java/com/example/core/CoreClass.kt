e0a7c4bda86b8dbf1b6766877b952c7f2da4a95f
roman_aimaletdinov <r.aimaletdinov@indriver.com>
2022-03-29 16:56:01 +0300

    fix

--- core/src/main/java/com/example/core/CoreClass.kt
+++ core/src/main/java/com/example/core/CoreClass.kt
@@ -5,13 +5,13 @@ import android.util.Log
 class CoreClass {
 
     fun getFeature(): String {
-        Thread.sleep(5000)
+        //Thread.sleep(5000)
         // ch
         return "CoreClass"
     }
 
     fun getFeature2(): String {
-        Thread.sleep(5000)
+        //Thread.sleep(5000)
         return "CoreClass2"
     }
 }
--- first-feature/src/main/java/com/example/first_feature/FirstFeature.kt
+++ first-feature/src/main/java/com/example/first_feature/FirstFeature.kt
@@ -3,13 +3,13 @@ package com.example.first_feature
 class FirstFeature {
 
     fun getFeature(): String {
-        Thread.sleep(5000)
+        //Thread.sleep(5000)
         // kek
         return "getFeature"
     }
 
     fun getFeature2(): String {
-        Thread.sleep(5000)
+        //Thread.sleep(5000)
         return "getFeature2"
     }
 }
