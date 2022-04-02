b8273c523ce30e4e80cd9f97182931f984606ef9
roman_aimaletdinov <r.aimaletdinov@indriver.com>
2022-04-02 22:56:22 +0300

    выключил

--- buildSrc/src/main/java/ImpactAnalysisPlugin.kt
+++ buildSrc/src/main/java/ImpactAnalysisPlugin.kt
@@ -111,7 +111,7 @@ class ImpactAnalysisPlugin : Plugin<Project> {
                     project.tasks.findByPath(path)?.onlyIf {
                         val v = AffectedModuleDetector.isProjectAffected(project)
                         System.err.println("ROMAN: afterEvaluate -> project: ${project.name}, isAffect: $v")
-                        false
+                        v
                     }
                 }
             }
